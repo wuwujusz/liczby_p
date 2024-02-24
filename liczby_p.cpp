76474f9566982c87b6a407ded2a341eb4166ea11
wuwujusz <miqit07@gmail.com>
2024-02-24 10:00:28 +0100

    Create liczby_p.cpp

---
+++ liczby_p.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+void ppp(bool *numbersTable, int n)
+{
+    for (int i = 2; i*i <= n; ++i )
+    {
+        if (numbersTable[i] == true)
+            continue;
+        for (int j = 2*i ; j <= n; j += i)
+            numbersTable[j] = true;
+    }
+}
+int main()
+{
+    bool tablica[100000] = {0};
+    int a, n;
+    cin >> n;
+    tablica[1] = 1;
+    ppp(tablica, 100000);
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a;
+        if(tablica[a] == false) cout << "TAK";
+        else cout << "NIE";
+        cout << endl;
+    }
+}
